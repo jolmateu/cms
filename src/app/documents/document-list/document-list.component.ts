183ef65e36f60a04cc815351e0b8ca8a5bef390b
jolmateu <jolmateu@byui.edu>
2022-01-28 23:21:48 -0500

    Week 04
    Week 04  - Directives

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(0o1, "Test File 01", "This is a test file", "http://test01.txt", []),
+    new Document(0o2, "Test File 02", "This is another test file", "http://test02.txt", []),
+    new Document(0o3, "Test File 03", "This is a third test file", "http://test03.txt", []),
+    new Document(0o4, "Test File 04", "This is a fourth test file", "http://test04.txt", []),
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
